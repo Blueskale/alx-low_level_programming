2a2dade1483c1c4b879e5b8111cc535fa15c7a04
Blueskale <princewillmmadu@gmail.com>
2022-12-08 15:24:47 -0800

    101-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,23 @@
-nclude <stdio.h>
+#include <stdio.h>
+
+#include <string.h>
+
 #include <unistd.h>
-/**
- *   main - Prints out the last part of a quote in the standard error.
- *   Return: 1 if success.
+/** 
+ * main - rints out the last part of a quote in the standard error.
+ * eturn: 1 if success.
  */
+
 int main(void)
+
 {
-		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-			return (1);
-}
+    int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+    write(2,
+          "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+       	  s);
+    return (1);
+
+ }
+
+
