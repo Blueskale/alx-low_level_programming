fa4969a650a9c66ad38fdadc4a65e4f5189dc397
Blueskale <princewillmmadu@gmail.com>
2023-01-11 08:13:30 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-#include <string.h>
-
-#include <unistd.h>
-/** 
- * main - rints out the last part of a quote in the standard error.
- * eturn: 1 if success.
- */
-
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
-
 {
-    int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
-    write(2,
-          "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-       	  s);
-    return (1);
-
- }
-
-
+	return (1);
+}
